c7bf940f5f0209dd60ccdb6a55e7c4da3a5440d9
Alexander Book <alexander.book@frm2.tum.de>
2016-12-07 15:24:40 +0100

    adding lock, serial and communication error

---
+++ e21_util/transport.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2016, see AUTHORS.md
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from slave.transport import Serial
+
+class Serial(slave.transport.Serial):
+    def __init__(self, *args, **kwargs):
+        super(Serial, self).__init__(*args, **kwargs)
+
+    def get_name(self):
+        return self._serial.name
