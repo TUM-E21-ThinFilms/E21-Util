ff86493ed541bc58145dae9c81bd8688df439b2f
Alexander Book <alexander.book@frm2.tum.de>
2016-12-07 15:28:44 +0100

    add missing import

--- e21_util/transport.py
+++ e21_util/transport.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import slave
 from slave.transport import Serial
 
 class Serial(slave.transport.Serial):
