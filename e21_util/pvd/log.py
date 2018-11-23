dc35fc898c84ff653be86e9a7646eb1396999321
Alexander Book <alexander.book@frm2.tum.de>
2018-11-23 15:33:37 +0100

    adding paths and log

---
+++ e21_util/pvd/log.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2016, see AUTHORS.md
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import logging
+from logging.handlers import WatchedFileHandler
+from e21_util.pvd.paths import Paths
+
+class PVDFileHandler(WatchedFileHandler):
+    def __init__(self, filename):
+        # The logs are placed in this folder, since a logrotate service is maintained here.
+        super(SputterFileHandler, self).__init__(Paths.LOG_PATH+filename)
+
+def get_pvd_logger(name, filename):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh = SputterFileHandler(filename)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
